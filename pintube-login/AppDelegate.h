ea5aea165f4f2041484d10a978014b20caa0ea3a
tko <tko@tko-macbookpro.roam.corp.google.com>
2014-06-24 16:10:13 -0700

    Initial Commit

---
+++ pintube-login/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pintube-login
+//
+//  Created by Taeho Ko on 6/24/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
